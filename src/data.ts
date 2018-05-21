aa24d09ba3e9ce49a2bb13162503f26a75ef7f75
Mike Chen <mike@mikechen.is>
2018-05-21 00:59:22 -0700

    Add more cards

--- src/data.ts
+++ src/data.ts
@@ -7,8 +7,42 @@ interface Card {
 
 export const cards: Card[] = [{
   name: 'The Fool',
-  textName: 'The Fool',
   upright: "The major arcana tell the story of an individual's path towards completion. Being the first card, The Fool therefore is a blank slate, a representation of purity and innocence of a child that knows nothing of the world. New journeys are signaled here, and The Fool is full of exuberance and energy. He does not know the dangers that can beset him during his travels, and thus he stumbles forward with complete optimism, never suspecting that he may be walking on a thin tightrope.",
   reversed: 'In your excitement to start a new journey, you have not heeded the dangers that surround you. This card suggests excessive eagerness and no awareness of consequences. You must plan more carefully your journey ahead. Who else do your actions affect?',
   url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-fool-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The Magician',
+  upright: 'The Magician is the representation of pure willpower. She takes all that is available to her and molds and transforms it to her desires. With her hand pointing to the ground and her wand pointing to the sky, she is the manifestation of the ancient phrase "as above so below"—meaning that earth reflects heaven, what is within is also without, the microcosm is the macrocosm. Naturally, this phrase is the origin of what is commonly understood as "magic."',
+  reversed: 'There are those that use their abilities for continual self-gain and through that weave illusions and lies. A reversal here might indicate that you are getting further and further away from the truth.',
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-magician-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The High Priestess',
+  upright: "To direct your will outward, one must also be able to understand one's internal world. The High Priestess is the guardian of the unconscious and beckons the traveler to the world within.",
+  reversed: 'The High Priestess Reversed calls for you to listen closely to what your inner self tells you. There was a time in which you heard her, but along the twists and turns along the path of life, her voice has been silenced somehow. What parts of your unconscious are you denying? What is it trying to say?',
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-high-priestess-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The Empress',
+  upright: 'She is the symbol of the feminine principle—a representation of Venus and mother earth. She is nurturing, fertile, and a provider. There is something to her that suggests a mother figure as well.',
+  reversed: 'Though normally the Empress is incredibly nurturing, the dark side of that is that she can also be smothering. Allowing something to grow means also giving it room to explore and expand. what parts of your life could you benefit from letting go?',
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-empress-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The Emperor',
+  upright: 'While the Empress represents the archetypical mother, the Emperor represents the father. He exudes authority—and though he is stern, he is also filled with wisdom and understanding from his years. He signals structure, stability, and the rule of thought over the heart.',
+  reversed: 'A good emperor not only understands when to take control but also when the control is too much. In this case, the authority that he exudes has turned to domination, his firm grasp of his kingdom turning into suffering. What parts of yourself could you aim to release?',
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-emperor-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The Hierophant',
+  upright: 'Following the bounds of tradition. What is tried-and-true is valued, and it is not the time to stray from the path. This card can also represent the pursuit of knowledge or a spiritual guide.',
+  reversed: 'You might be feeling incredibly restricted in your life, and questioning whether the decisions that have been made were made with your true self in mind, or rather what others expect of you. You are considering embarking on the road less taken. Free yourself and diverge from the norms which society expects of you, and pursue a new approach in life.',
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-hierophant-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The Lovers',
+  upright: "A union of harmony full of trust, confidence, and strength. This relationship is one that represents both a physical attraction and a deep emotional bond between them. It can also represent an inner harmony between two aspects of one's personality: anima and animus, yin and yang, and so forth.",
+  reversed: 'In a partnership, there may be a sense of feeling like one party is giving or taking too much. Are the feelings in your relationship mutual? Or conversely, there might be disagreements—between either two parts within yourself, or in your relationships.',
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-lovers-meaning-major-arcana-tarot-card-meanings',
+}, {
+  name: 'The Chariot',
+  upright: 'At times, success and victory is the result of pure willpower and self-control. To achieve and accomplish what you desire, you must take the reins and harness the chaotic forces that surround you.',
+  reversed: "Being at the mercy of two opposing forces, constantly battling for domination. There is a sense that you have lost control here, that you are not at the driver's seat, that you are a passive observer. You must begin to understand what it is in your life that you can and cannot drive, and let what you cannot go.",
+  url: 'https://labyrinthos.co/blogs/tarot-card-meanings-list/the-chariot-meaning-major-arcana-tarot-card-meanings',
 }];
